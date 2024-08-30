d9c31a2ba163fda05ebe22b92d9a7ad07159b85a
clx <meng_yme@163.com>
2024-08-30 09:07:01 +0800

    init -demo

---
+++ clxmm-test/src/main/java/org/clxmm/HelloWorld.java
@@ -0,0 +1,20 @@
+package org.clxmm;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author clxmm
+ * @Description
+ * @create 2024-08-30 09:01
+ */
+public class HelloWorld {
+
+
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+        logger.info("Hello World");
+    }
+
+
+}
